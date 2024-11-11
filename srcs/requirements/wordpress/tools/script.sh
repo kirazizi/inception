0ecef0d3187dad2d2fa9913a4761f26d291c5c0b
SMAIL KIRA <108758834+kirazizi@users.noreply.github.com>
2024-11-11 18:49:35 +0100

    add a env and fix some problemes

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 
 mkdir -p /var/www/html/wordpress
-mkdir -p /run/php/ && chown -R www-data:www-data /run/php/
+mkdir -p /run/php/ 
+chown -R www-data:www-data /run/php/
+
+sleep 10
 
 cd /var/www/html/wordpress
 
-if [ ! -f wp-config.php ]; then
+# if [ ! -f wp-config.php ]; then
 
-wp core download --allow-root --path=/var/www/html/wordpress
+wp core download --allow-root 
 
-wp config create --dbname=wordpress --dbuser=admin --dbpass=admin --dbhost=mariadb --allow-root --path=/var/www/html/wordpress
-wp core install --url=https://sbzizal.42.fr --title=wordpress --admin_user=admin --admin_password=admin --admin_email=admin@mail.com --skip-email --allow-root --path=/var/www/html/wordpress
-wp user create user user@mail.com --role=author --user_pass=user --allow-root
+mv wp-config-sample.php wp-config.php
 
-fi
+# change the database name in the line 23
+sed -i "s/database_name_here/wordpress/1" wp-config.php
+
+# change the database user in the line 26
+sed -i "s/username_here/admin/1" wp-config.php
+
+# change the database password in the line 29
+sed -i "s/password_here/admin/1" wp-config.php
+
+# change the database host in the line 32 (to connect to the mariadb container)
+sed -i "s/localhost/mariadb/1" wp-config.php
+
+wp core install \
+    --url=localhost \
+    --title=wordpress \
+    --admin_user=admin \
+    --admin_password=admin \
+    --admin_email=admin@mail.com \
+    --skip-email \
+    --allow-root \
+
+wp user create user user@mail.com \
+    --role=author \
+    --user_pass=user \
+    --allow-root
+
+# fi
 
 sed -i 's/^listen = .*$/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 
