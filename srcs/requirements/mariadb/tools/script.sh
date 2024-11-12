e1aa3c8a9767c0c562f62fb7333f257088f0fd97
kirazizi <smailzizi66@gmail.com>
2024-11-12 12:43:54 -0500

    adding to git

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,10 @@
 
 service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" > db.sql
-echo "CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';" >> db.sql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%';" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';" >> db.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD';" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> db.sql
+echo "ALTER USER '$RT_USER'@'localhost' IDENTIFIED BY '$RT_PASSWRD';" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
 mysql < db.sql
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,40 +8,35 @@ sleep 10
 
 cd /var/www/html/wordpress
 
-# if [ ! -f wp-config.php ]; then
-
 wp core download --allow-root 
 
 mv wp-config-sample.php wp-config.php
 
 # change the database name in the line 23
-sed -i "s/database_name_here/wordpress/1" wp-config.php
+sed -i "s/database_name_here/$DB_NAME/1" wp-config.php
 
 # change the database user in the line 26
-sed -i "s/username_here/admin/1" wp-config.php
+sed -i "s/username_here/$DB_USER/1" wp-config.php
 
 # change the database password in the line 29
-sed -i "s/password_here/admin/1" wp-config.php
+sed -i "s/password_here/$DB_PSSWRD/1" wp-config.php
 
 # change the database host in the line 32 (to connect to the mariadb container)
 sed -i "s/localhost/mariadb/1" wp-config.php
 
 wp core install \
-    --url=localhost \
-    --title=wordpress \
-    --admin_user=admin \
-    --admin_password=admin \
-    --admin_email=admin@mail.com \
-    --skip-email \
+    --url=$DOMAIN_NAME \
+    --title=$WP_NAME \
+    --admin_user=$WP_ADMIN \
+    --admin_password=$WP_PSSWRD \
+    --admin_email=$AD_EMAIL \
     --allow-root \
 
-wp user create user user@mail.com \
+wp user create $WP_USR $WP_EMAIL \
     --role=author \
-    --user_pass=user \
+    --user_pass=$WP_PWD \
     --allow-root
 
-# fi
-
-sed -i 's/^listen = .*$/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/^listen = .*$/listen = 9000/" /etc/php/7.4/fpm/pool.d/www.conf
 
 php-fpm7.4 -F
